1967d9601df3f5ad74128902c749936c7eca7c5a
kruban <kruban@digital-spirit.ru>
2024-04-20 14:07:58 +0300

    feat: add prettier config

--- src/app/app/config/app.config.ts
+++ src/app/app/config/app.config.ts
@@ -1,13 +1,10 @@
-import {importProvidersFrom} from '@angular/core';
-import {ApplicationConfig} from '@angular/platform-browser';
-import {provideRouter} from '@angular/router';
-import {routes} from '@app/config/app.routes';
-import {HttpClientModule} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from '@app/config/app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
-  ],
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule, BrowserAnimationsModule)],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {AppComponent} from '@app/app.component';
-import {appConfig} from '@app/config/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from '@app/app.component';
+import { appConfig } from '@app/config/app.config';
 
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
