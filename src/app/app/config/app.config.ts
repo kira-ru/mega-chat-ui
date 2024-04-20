aa489dddda15f05257b6ddf3d9a46eb426ba1191
kruban <kruban@digital-spirit.ru>
2024-04-20 13:52:09 +0300

    feat: angular project init

---
+++ src/app/app/config/app.config.ts
@@ -0,0 +1,13 @@
+import {importProvidersFrom} from '@angular/core';
+import {ApplicationConfig} from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
+import {routes} from '@app/config/app.routes';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+  ],
+};
